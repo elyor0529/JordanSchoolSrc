0c132ce5d802af9c53fbbf67cbab4550c6d3d370
Yaseen <yasjbr@yahoo.com>
2019-07-24 19:01:36 +0300

    2707-07

--- app/pages/financial/payment/payment-dialog/payment-dialog.component.ts
+++ app/pages/financial/payment/payment-dialog/payment-dialog.component.ts
@@ -1,6 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, Inject } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { DatePipe } from '@angular/common';
+import { RegParentService } from 'src me finnancial/app/pages/Reg/parents/reg-parent.service';
+import { Lkplookup } from 'src/app/Models/Lookups/lkplookup';
+import { LookupTypes } from 'src/app/Models/Enum/SystemEnum';
+import { LookupsApiService } from 'src/app/pages/lookups/lookups-api.service';
+import { ValidationBase } from 'src/app/validationBase';
+import { PaymentService } from '../payment.service';
+import { Payment } from 'src/app/Models/financial/payment';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 
 @Component({
   selector: 'app-payment-dialog',
@@ -11,14 +19,29 @@ import { DatePipe } from '@angular/common';
 export class PaymentDialogComponent implements OnInit {
 
   public form: FormGroup;
-  public now:Date =new Date();
+  parentList: any; 
   public myDate = new Date();
-  public dateFormatted:string;
+  public dateFormatted: string;
+  voucherTypeList:Lkplookup[];
+  VoucherStatusList:Lkplookup[];
+  loading = false;
+  edit=false;
+  id: number;
+  public dialogRef: MatDialogRef<PaymentDialogComponent>;
+  @Output() event=new EventEmitter<Payment>(true);
+
+  
 
   constructor(
     private fb: FormBuilder,
-    private datePipe: DatePipe
-  ) { 
+    private datePipe: DatePipe,
+    public validator: ValidationBase,
+    private parentService: RegParentService,
+    private lookupService: LookupsApiService,
+    private service: PaymentService,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {
+    this.initForm();
     this.dateFormatted = this.datePipe.transform(this.myDate, 'yyyy-MM-dd');
 
   }
@@ -28,26 +51,103 @@ export class PaymentDialogComponent implements OnInit {
     this.form = this.fb.group(
       {
         id: [0],
-        RegParentId:['',[Validators.required]],
-        voucherId:[0],
-        voucherDate:[this.dateFormatted],
-        voucherTypeId:[],
-        voucherStatusId:[],
-        debit:[0],
-        credit:[0],
-        note:['add your note']
+        RegParentId: ['', [Validators.required]],
+        voucherId: [0],
+        voucherDate: [this.dateFormatted],
+        voucherTypeId: [],
+        voucherStatusId: [],
+        debit: [0],
+        credit: [0],
+        note: ['add your note']
       }
     );
   }
 
-  ngOnInit() {
-     this.initForm();
+  getParentList() {
+    return this.parentService.getParentsList()
+      .subscribe(res => (this.parentList = res));
   }
 
-  currentDate() {
-    const currentDate = new Date();
-    return currentDate.toISOString().substring(0,10);
-  }
 
   
+  setupUpdate() {
+    console.log(this.data);
+    if (!this.data.id) return;
+    this.id = this.data.id;
+      this.edit = true;
+      this.loading = true;
+      this.service.getPaymentById(this.id).subscribe(res => {
+        this.form = this.validator.patchForm(this.form, res);
+      }, err => console.log(err),
+        () => this.loading = false);
+  }
+
+  closeDialog(): void {
+    this.dialogRef.close();
+  }
+
+
+
+  private getLookups() {
+    this.lookupService.getLookupsByType2([
+      LookupTypes.VoucherType,
+      LookupTypes.VoucherStatus
+    ])
+      .subscribe(
+        res => {this.fillLookups(res)},
+        _err => { console.log("Error") },
+        () => { console.log("complete")
+        });
+  }
+
+  private fillLookups(data: any) {
+    data.forEach((element: Lkplookup[]) => {
+      switch (element[0].typeId) {
+        case LookupTypes.VoucherType:this.voucherTypeList = element; break;
+        case LookupTypes.VoucherStatus:this.VoucherStatusList = element; default: break;
+      }
+    });
+  }
+
+
+  
+  submit() {
+    if (!this.form.valid) {
+      this.validator.markFormTouched(this.form);
+      return;
+    }
+    this.loading = true;
+    this.edit ? this.updatePayment() : this.addPayment();
+  }
+
+  addPayment() {
+    this.service.addPayment(this.form.value).subscribe(
+      res => {
+       this.event.emit(this.form.value);
+        this.dialogRef.close(this.form.value);
+      },
+      err => console.log('errrrrrr'+err)
+    );
+  }
+
+  updatePayment() {
+    console.log('update');
+    this.service.updatePayment(this.id, this.form.value).subscribe(
+      res => {
+        this.dialogRef.close(this.form.value);
+      //  this.router.navigateByUrl(this.returnUrl);
+      },
+      err => console.log(err)
+    );
+  }
+
+  ngOnInit() {
+    this.setupUpdate();
+    this.getParentList();
+    this.getLookups();
+  }
+
+
+
+
 }
--- app/pages/financial/payment/payment.service.ts
+++ app/pages/financial/payment/payment.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Payment } from 'src/app/Models/financial/payment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -17,4 +18,22 @@ export class PaymentService {
   getPaymentList(){
    return this.http.get<Payment[]>(this.apiUrl,environment.httpOptions);
   }
+
+  
+  getPaymentById(id: number): Observable<Payment> {
+    return this.http.get<Payment>(`${this.apiUrl}/${id}`, environment.httpOptions);
+  }
+
+  addPayment(model: Payment): Observable<Payment> {
+    return this.http.post<Payment>(this.apiUrl, model, environment.httpOptions);
+  }
+
+  deletePayment(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`, environment.httpOptions)
+  }
+
+  updatePayment(id:number, model:Payment):Observable<void>{
+    return this.http.put<void>(`${this.apiUrl}/${id}`,model,environment.httpOptions);
+  }
+
 }
